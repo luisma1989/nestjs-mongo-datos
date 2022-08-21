fc0882fc29fd67601bfbb97576542701ea06e117
luisma <luis.fernandez@kodit.io>
2022-08-21 22:21:12 +0200

    feat: generate jwt

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from 'src/users/services/users.service';
+import { User } from 'src/users/entities/user.entity';
+import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
@@ -16,5 +23,13 @@ export class AuthService {
       }
     }
     return null;
+  }
+
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
