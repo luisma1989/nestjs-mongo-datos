c89b2fb3fa635463442bda8313d5facb834a773f
luisma <luis.fernandez@kodit.io>
2022-08-21 22:36:38 +0200

    feat: generate token with login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
 
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './controllers/auth.controller';
 
+import config from 'src/config';
+
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => ({
+        secret: configService.jwt.secret,
+        signOptions: { expiresIn: configService.jwt.expiresIn },
+      }),
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     const payload: PayloadToken = { role: user.role, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
-      user,
+      ...user,
     };
   }
 }
