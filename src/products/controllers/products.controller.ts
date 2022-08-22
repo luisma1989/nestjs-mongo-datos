bee38f98910efd907deb0e99dfc86c4ae0004a2e
luisma <luis.fernandez@kodit.io>
2022-08-22 13:21:01 +0200

    feat: add jwt guard

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -12,9 +12,8 @@ import {
   UseGuards,
   // ParseIntPipe,
 } from '@nestjs/common';
-import { Response } from 'express';
+// import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
 
 import { MongoIdPipe } from '../../common/mongo-id.pipe';
 import {
@@ -23,14 +22,17 @@ import {
   FilterProductsDto,
 } from '../dtos/products.dtos';
 import { ProductsService } from './../services/products.service';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
+  @Public()
   @ApiOperation({ summary: 'List of products' })
   getProducts(@Query() params: FilterProductsDto) {
     return this.productsService.findAll(params);
@@ -41,6 +43,7 @@ export class ProductsController {
     return `yo soy un filter`;
   }
 
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', MongoIdPipe) productId: string) {
